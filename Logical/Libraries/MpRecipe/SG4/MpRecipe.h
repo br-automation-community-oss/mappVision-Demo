15149ee62286011d51e0ab7dc86d83ac17f9147a
Stephan Stricker <stephan.stricker@br-automation.com>
2024-09-12 10:14:50 +0200

    Update mapp versions

--- Logical/Libraries/MpRecipe/SG4/MpRecipe.h
+++ Logical/Libraries/MpRecipe/SG4/MpRecipe.h
@@ -1,6 +1,6 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
-/* MpRecipe 5.24.2 */
+/* MpRecipe 5.24.3 */
 
 #ifndef _MPRECIPE_
 #define _MPRECIPE_
@@ -9,7 +9,7 @@ extern "C"
 {
 #endif
 #ifndef _MpRecipe_VERSION
-#define _MpRecipe_VERSION 5.24.2
+#define _MpRecipe_VERSION 5.24.3
 #endif
 
 #include <bur/plctypes.h>
